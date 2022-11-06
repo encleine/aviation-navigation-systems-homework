126505cb1796abb95f30c46a46172631bae7c046
enc <aidenblankroot@gmail.com>
2022-11-06 17:47:51 +0300

    small edit to stupify it

--- التجربة الثالثة/index.ino
+++ التجربة الثالثة/index.ino
@@ -1,14 +1,15 @@
 
 
-int led =11;
-
+int led = 9;
 void setup() {
     pinMode(led, OUTPUT);
     Serial.begin(9600);
 }
 
 void loop() {
-    int ADCin_value = analogRead(A0);
-    digitalWrite(led, ADCin_value > 650);
+    int ADCin = analogRead(A0);
+    int ADCout = map(ADCin ,0,1023,0,255);
+    if (ADCin > 650)
+        analogWrite(led, ADCout);
 }
 
