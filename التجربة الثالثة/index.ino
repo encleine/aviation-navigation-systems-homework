ca4503157f1567e1c10a11d7e978e64e81ce82ab
enc <aidenblankroot@gmail.com>
2022-11-06 17:49:03 +0300

    a mistake

--- التجربة الثالثة/index.ino
+++ التجربة الثالثة/index.ino
@@ -7,8 +7,8 @@ void setup() {
 }
 
 void loop() {
-    int ADCin = analogRead(A0);
-    int ADCout = map(ADCin ,0,1023,0,255);
+    float ADCin = analogRead(A0);
+    float ADCout = map(ADCin ,0,1023,0,255);
     if (ADCin > 650)
         analogWrite(led, ADCout);
 }
